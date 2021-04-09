76f44979731fc3241171631a57caa3a9fc26f502
naufal <naufalrandialdiansyah@gmail.com>
2021-04-09 16:56:04 +0700

    update datatable bgr kota

--- src/Model/ind_provinsi.php
+++ src/Model/ind_provinsi.php
@@ -21,4 +21,38 @@ class ind_provinsi extends Model
     		return $query->get();
     	}
     }
+    public function scopeDatatable($query,$request,$page=12)
+    {
+        $search       = ["nama"];
+        $searchqry    = '';
+
+        $searchqry = "(";
+        foreach ($search as $key => $value) {
+            if($key == 0){
+                $searchqry .= "lower($value) like '%".strtolower($request->search)."%'";
+            }else{
+                $searchqry .= "OR lower($value) like '%".strtolower($request->search)."%'";
+            }
+        }
+
+        $searchqry .= ")";
+        if(@$request->sort_by != null){
+            $explode = explode('.', $request->sort_by);
+                $query->orderBy($explode[0],$explode[1]);
+        }else{
+                $query->orderBy('created_at','desc');
+        }
+        $query->whereRaw($searchqry);
+
+        if($request->get == 'all'){
+            if(!empty($request->id)){
+                return $query->find($request->id);
+            }else{
+                return $query->get();
+            }
+        }else{
+                return $query->paginate($page);
+        }
+
+    }
 }
